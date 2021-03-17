11a443c5c5086d6b2d9947d5404b71a7293f87c2
Luke Fantom <luke.fantom@gmail.com>
2021-03-17 22:40:07 +0000

    updated styling & added js

--- public/jet-2.bundle.js
+++ public/jet-2.bundle.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
@@ -16,17 +15,18 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ \"./src/index.scss\");\n\n\n//# sourceURL=webpack://jet-2/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.scss */ \"./src/styles/main.scss\");\n\n/* ----------------Manage modals----------------*/\n\nvar manageModals = function manageModals() {\n  // Create a random id to locate each modal\n  var id = Math.floor(Math.random() * 100); // Select overlay container and modal template from DOM\n\n  var overlay = document.getElementById(\"overlay\");\n  var modalTemplate = document.getElementById(\"modal-template\"); // Clone the modal template & append with unique id\n\n  var modal = modalTemplate.cloneNode(true);\n  modal.id = id;\n  modal.classList.remove(\"hidden\"); //  Select continue/cancel button from DOM\n  //  Append with unique id & add event listener\n\n  var continueButton = modal.querySelector(\"#continue\");\n  continueButton.id = id;\n  continueButton.addEventListener(\"click\", removeModal);\n  var cancelButton = modal.querySelector(\"#cancel\");\n  cancelButton.id = id;\n  cancelButton.addEventListener(\"click\", removeModal); // Append overlay container with new modal\n\n  overlay.appendChild(modal);\n};\n/* ----------------Remove modal function----------------*/\n\n\nvar removeModal = function removeModal(event) {\n  var id = event.target.id;\n  var modal = document.getElementById(id);\n  modal.remove();\n};\n/* ----------------Add modal button----------------*/\n\n\nvar addModalButton = document.getElementById(\"add-modal\");\naddModalButton.addEventListener(\"click\", manageModals);\n\n//# sourceURL=webpack://jet-2/./src/index.js?");
 
 /***/ }),
 
-/***/ "./src/index.scss":
-/*!************************!*\
-  !*** ./src/index.scss ***!
-  \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ "./src/styles/main.scss":
+/*!******************************!*\
+  !*** ./src/styles/main.scss ***!
+  \******************************/
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://jet-2/./src/index.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Can't find stylesheet to import.\\n   ╷\\n15 │ @import \\\"character-styles\\\";\\n   │         ^^^^^^^^^^^^^^^^^^\\n   ╵\\n  src/styles/main.scss 15:9  root stylesheet\\n    at processResult (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/webpack/lib/NormalModule.js:649:19)\\n    at /Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/webpack/lib/NormalModule.js:743:5\\n    at /Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/loader-runner/lib/LoaderRunner.js:399:11\\n    at /Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/loader-runner/lib/LoaderRunner.js:251:18\\n    at context.callback (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\\n    at /Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/sass-loader/dist/index.js:54:7\\n    at Function.call$2 (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/sass/sass.dart.js:91729:16)\\n    at _render_closure1.call$2 (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/sass/sass.dart.js:80373:12)\\n    at _RootZone.runBinary$3$3 (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/sass/sass.dart.js:27269:18)\\n    at _FutureListener.handleError$1 (/Users/work/School-of-Code/post-bootcamp/jet-2/node_modules/sass/sass.dart.js:25797:19)\");\n\n//# sourceURL=webpack://jet-2/./src/styles/main.scss?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import "./index.scss";
+import "./styles/main.scss";
+
+/* ----------------Manage modals----------------*/
+const manageModals = () => {
+  // Create a random id to locate each modal
+  const id = Math.floor(Math.random() * 100);
+
+  // Select overlay container and modal template from DOM
+  const overlay = document.getElementById("overlay");
+  const modalTemplate = document.getElementById("modal-template");
+
+  // Clone the modal template & append with unique id
+  const modal = modalTemplate.cloneNode(true);
+  modal.id = id;
+  modal.classList.remove("hidden");
+
+  //  Select continue/cancel button from DOM
+  //  Append with unique id & add event listener
+  const continueButton = modal.querySelector("#continue");
+  continueButton.id = id;
+  continueButton.addEventListener("click", removeModal);
+
+  const cancelButton = modal.querySelector("#cancel");
+  cancelButton.id = id;
+  cancelButton.addEventListener("click", removeModal);
+
+  // Append overlay container with new modal
+  overlay.appendChild(modal);
+};
+
+/* ----------------Remove modal function----------------*/
+const removeModal = (event) => {
+  const id = event.target.id;
+  const modal = document.getElementById(id);
+  modal.remove();
+};
+
+/* ----------------Add modal button----------------*/
+const addModalButton = document.getElementById("add-modal");
+addModalButton.addEventListener("click", manageModals);
