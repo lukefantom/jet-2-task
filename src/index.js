ce1052fc5d86dc88f8074a3803bc54c341c266a3
Luke Fantom <luke.fantom@gmail.com>
2021-03-18 10:49:03 +0000

    updated so that modals layer over previous

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import "./styles/main.scss";
 
+/* ----------------Modal Counter----------------*/
+const modalCounter = document.getElementById("modal-counter");
+
 /* ----------------Manage modals----------------*/
 const manageModals = () => {
   // Create a random id to locate each modal
   const id = Math.floor(Math.random() * 100);
 
-  // Select overlay container and modal template from DOM
-  const overlay = document.getElementById("overlay");
-  const modalTemplate = document.getElementById("modal-template");
+  // Select container and modal template from DOM
+  const container = document.getElementById("modal-container");
+  const template = document.getElementById("template");
 
   // Clone the modal template & append with unique id
-  const modal = modalTemplate.cloneNode(true);
+  const modal = template.cloneNode(true);
   modal.id = id;
-  modal.classList.remove("hidden");
+  modal.classList.remove("overlay--hidden");
 
   //  Select continue/cancel button from DOM
   //  Append with unique id & add event listener
@@ -25,7 +28,11 @@ const manageModals = () => {
   cancelButton.addEventListener("click", removeModal);
 
   // Append overlay container with new modal
-  overlay.appendChild(modal);
+  container.appendChild(modal);
+
+  // Count the number of modals on page & update modal count text
+  const numberOfModals = document.getElementsByClassName("modal").length - 1;
+  modalCounter.innerText = `Modal Count: ${numberOfModals}`;
 };
 
 /* ----------------Remove modal function----------------*/
@@ -33,6 +40,10 @@ const removeModal = (event) => {
   const id = event.target.id;
   const modal = document.getElementById(id);
   modal.remove();
+
+  // Decrement the count every time a modal is removed
+  const numberOfModals = document.getElementsByClassName("modal").length - 1;
+  modalCounter.innerText = `Modal Count: ${numberOfModals}`;
 };
 
 /* ----------------Add modal button----------------*/
